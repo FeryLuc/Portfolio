558bd0dd22d312e6b702e844caf4b1c362ce757e
GrasPanda <Lucasus822@gmail.com>
2026-01-10 02:04:35 +0100

    Feature aside  + contenu  A propos

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
 //Mobile nav
 //Toggle
-const mobileToggle = document.getElementById('mobile-toggle');
-const mobileNav = document.getElementById('mobile-nav');
-mobileToggle.addEventListener('click', () =>
-  mobileNav.classList.toggle('hidden')
+const mobileToggle = document.getElementById("mobile-toggle");
+const mobileNav = document.getElementById("mobile-nav");
+mobileToggle.addEventListener("click", () =>
+  mobileNav.classList.toggle("hidden")
 );
 //Fermer la nav au clique d'un lien
-document.querySelectorAll('#mobile-nav a').forEach((link) => {
-  link.addEventListener('click', () => {
-    mobileNav.classList.add('hidden');
+document.querySelectorAll("#mobile-nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.add("hidden");
   });
 });
 
 //Formulaire CTA
 //Activation
-const formInputs = document.querySelectorAll('input, textarea');
-const submitBtn = document.getElementById('submit-btn');
+const formInputs = document.querySelectorAll("input, textarea");
+const submitBtn = document.getElementById("submit-btn");
 function checkFormState() {
   const isAllFilled = Array.from(formInputs).every(
-    (f) => f.value.trim() !== ''
+    (f) => f.value.trim() !== ""
   );
   submitBtn.disabled = !isAllFilled;
 
   if (isAllFilled) {
-    submitBtn.classList.add('active');
+    submitBtn.classList.add("active");
   } else {
-    submitBtn.classList.remove('active');
+    submitBtn.classList.remove("active");
   }
 }
-formInputs.forEach((i) => i.addEventListener('input', checkFormState));
+formInputs.forEach((i) => i.addEventListener("input", checkFormState));
 //Reset inputs après l'action FormSpree
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener("click", () => {
   setTimeout(() => {
-    formInputs.forEach((i) => (i.value = ''));
+    formInputs.forEach((i) => (i.value = ""));
   }, 200);
 });
 
 //Scroll
 //Intersection Observer for fade-in at ~30% visibility
-const sections = document.querySelectorAll('.fade-section');
+const sections = document.querySelectorAll(".fade-section");
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio > 0.3)
-        entry.target.classList.add('fade-visible');
+        entry.target.classList.add("fade-visible");
     });
   },
   { threshold: [0, 0.3, 1] }
